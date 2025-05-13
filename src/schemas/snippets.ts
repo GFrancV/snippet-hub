67ab45ffb18ee98709cc47cdd3795ab99516e929
GFrancV <gfrancv@hotmail.com>
2025-05-14 00:03:34 +0100

    feat: update snippetFormSchema to enforce title length and add validation for description and code fields

--- src/schemas/snippets.ts
+++ src/schemas/snippets.ts
@@ -3,9 +3,13 @@ import { z } from "astro:content";
 export const snippetFormSchema = z.object({
   title: z
     .string()
-    .min(2, { message: "The title must have at least 2 characters" }),
-  description: z.string(),
-  code: z.string(),
+    .min(5, { message: "The title must have at least 5 characters" }),
+  description: z.string().max(500, {
+    message: "The description must have at most 500 characters",
+  }),
+  code: z.string().max(1000, {
+    message: "The code must have at most 1000 characters",
+  }),
   language: z.enum([
     "javascript",
     "python",
