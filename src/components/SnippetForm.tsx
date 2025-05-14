b5ffcecad0e531300ac2ffb2f65d2aff91c898f1
GFrancV <gfrancv@hotmail.com>
2025-05-14 10:14:36 +0100

    feat: add loading state and spinner to SnippetForm submission button

--- src/components/SnippetForm.tsx
+++ src/components/SnippetForm.tsx
@@ -33,11 +33,13 @@ export default () => {
     },
   ];
 
+  const [isLoading, setIsLoading] = useState(false);
   const [language, setLanguage] = useState<SnippetLanguages>("javascript");
   const [errors, setErrors] = useState<{ [key: string]: string }>({});
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setIsLoading(true);
     const formData = new FormData(e.currentTarget);
     const dataToSend = {
       title: formData.get("title") as string,
@@ -63,13 +65,13 @@ export default () => {
         });
         setErrors(formattedErrors);
       }
+      setIsLoading(false);
       return;
     }
 
-    const data = await response.json();
-    if (data.message) {
-      console.log(data.message);
-    }
+    setErrors({});
+    setIsLoading(false);
+    window.location.href = "/ruta-destino";
   };
 
   return (
@@ -120,7 +122,33 @@ export default () => {
         <CodeEditor language={language} />
       </div>
 
-      <button className="flex gap-1 border border-transparent back rounded-full px-4 py-2 transition text-white bg-[#1e1e1e] hover:bg-fuchsia-300/30 hover:border-fuchsia-200 cursor-pointer">
+      <button
+        className={`flex items-center gap-2 border border-transparent back rounded-full px-4 py-2 transition text-white bg-[#1e1e1e] hover:bg-fuchsia-300/30 hover:border-fuchsia-200 cursor-pointer duration-300 ${
+          isLoading && "opacity-75 cursor-progress"
+        }`}
+        disabled={isLoading}
+      >
+        {isLoading && (
+          <div role="status">
+            <svg
+              aria-hidden="true"
+              className="inline size-6 animate-spin text-gray-600 fill-gray-300"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+            <span className="sr-only">Loading...</span>
+          </div>
+        )}
         Submit
       </button>
     </form>
