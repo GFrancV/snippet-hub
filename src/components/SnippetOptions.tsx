2e782f3fe3634e0cd8d449184f6219977c22f4aa
GFrancV <gfrancv@hotmail.com>
2025-05-16 10:41:35 +0100

    feat: add SnippetOptions component for snippet management with delete functionality

---
+++ src/components/SnippetOptions.tsx
@@ -0,0 +1,101 @@
+import { useEffect, useRef, useState } from "react";
+
+interface Props {
+  id: string;
+}
+
+const SnippetOptions = ({ id }: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const toggleDropdown = () => setIsOpen(!isOpen);
+
+  const deleteSnippet = async () => {
+    const response = await fetch(`/api/snippets/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      console.log("Snippet deleted successfully");
+      window.location.href = "/";
+    } else {
+      console.error("Failed to delete snippet");
+    }
+  };
+
+  return (
+    <div ref={dropdownRef} className="relative inline-block text-left">
+      <div>
+        <button
+          onClick={toggleDropdown}
+          type="button"
+          className="inline-flex w-full rounded-md bg-[#1e1e1e] text-sm text-[#f1f1ef] shadow-xs ring-1 ring-[#f1f1ef]/5 ring-inset p-1 transition duration-300 cursor-pointer hover:ring-[#f1f1ef]/15"
+          aria-expanded={isOpen}
+          aria-haspopup="true"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+            <path d="M12 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+            <path d="M12 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+          </svg>
+          <span className="sr-only">Options</span>
+        </button>
+      </div>
+      {isOpen && (
+        <ul
+          className="absolute right-0 z-10 w-32 origin-top-right mt-0.5 rounded-md bg-[#1e1e1e] shadow-lg ring-1 ring-black/5  focus:outline-hidden"
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="menu-button"
+          tabIndex={-1}
+        >
+          <li
+            className="py-1 overflow-hidden transition duration-300 hover:bg-red-700/20"
+            role="none"
+          >
+            <button
+              onClick={deleteSnippet}
+              className="flex px-4 py-2 text-sm text-red-400 w-full cursor-pointer"
+              role="menuitem"
+              tabIndex={-1}
+            >
+              Delete
+            </button>
+          </li>
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default SnippetOptions;
