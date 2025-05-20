aa55c6b31c747e659f228be4b42a8421a4191b49
GFrancV <gfrancv@hotmail.com>
2025-05-20 21:56:18 +0100

    refactor: update site URL in astro.config.mjs for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mi-dominio.com",
+  site: "https://snippet-hub-psi.vercel.app",
   vite: {
     plugins: [tailwindcss()],
   },
